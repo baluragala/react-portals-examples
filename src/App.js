e6f1db3579b7cf5d7aa4db0ac94f844647fea808
Bala Krishna Ragala <baluragala@sparity.com>
2019-01-28 13:00:19 +0530

    2 examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import NewWindowPortal from "./NewWindowPortal";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      showWindowPortal: false
+    };
+
+    this.toggleWindowPortal = this.toggleWindowPortal.bind(this);
+  }
+
+  componentDidMount() {
+    window.setInterval(() => {
+      this.setState(state => ({
+        ...state,
+        counter: state.counter + 1
+      }));
+    }, 1000);
+  }
+
+  toggleWindowPortal() {
+    this.setState(state => ({
+      ...state,
+      showWindowPortal: !state.showWindowPortal
+    }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Counter: {this.state.counter}</h1>
+
+        <button onClick={this.toggleWindowPortal}>
+          {this.state.showWindowPortal ? "Close the" : "Open a"} Portal
+        </button>
+
+        {this.state.showWindowPortal && (
+          <NewWindowPortal>
+            <h1>Counter in a portal: {this.state.counter}</h1>
+            <p>Even though I render in a different window, I share state!</p>
+
+            <button onClick={() => this.setState({ showWindowPortal: false })}>
+              Close me!
+            </button>
+          </NewWindowPortal>
+        )}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Parent from "./Parent";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// example 1
+//ReactDOM.render(<App />, document.getElementById("root"));
+
+// example 2
+// These two containers are siblings in the DOM
+const appRoot = document.getElementById("app-root");
+ReactDOM.render(<Parent />, appRoot);
+//const modalRoot = document.getElementById('modal-root');
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
